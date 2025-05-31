42b0046c672be8905b39617b5869a560aef9ce48
harsh_sde <harsh_s.dev@outlook.com>
2025-05-31 19:56:31 +0530

    Practice question on OOPs

---
+++ ts/PracticeSets/5_practiceSet.ts
@@ -0,0 +1,27 @@
+class BankAccount {
+  private balance: number;
+
+  constructor(public accountNumber: string, initialBalance: number) {
+    this.balance = initialBalance;
+  }
+
+  deposit(amount: number): void {
+    this.balance += amount;
+  }
+
+  withdraw(amount: number): void {
+    if (amount > this.balance) {
+      throw new Error("Insufficient balance");
+    } else {
+      this.balance -= amount;
+    }
+  }
+
+  getBalance(): number {
+    return this.balance;
+  }
+}
+
+const userOne = new BankAccount("505123", 500000);
+
+console.log(userOne.getBalance()); // 50000
