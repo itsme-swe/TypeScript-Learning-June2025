6c1dfc7bf896b071918b0c0e6e770740f7d068fa
itsharsh_swe <harsh_s.dev@outlook.com>
2026-04-26 10:57:49 +0530

    update

--- ts/BasicsOfTypeScript/3_creatingObject.ts
+++ ts/BasicsOfTypeScript/3_creatingObject.ts
@@ -14,3 +14,28 @@ let obj1: { name: string; age: number; isLoggedIn: boolean } = {
   age: 32,
   isLoggedIn: false,
 };
+
+console.log(obj1);
+
+//🌟 Creating Object using interface --- we use interface to define data types for object
+
+interface StudentInterface {
+  name: string;
+  rollNo: number;
+  subject: string;
+  isPresent: boolean;
+}
+
+const App = () => {
+  const student: StudentInterface = {
+    name: "Harsh",
+    rollNo: 105,
+    subject: "CS",
+    isPresent: true,
+  };
+  console.log(student);
+};
+
+App();
+
+// { name: 'Harsh', rollNo: 105, subject: 'CS', isPresent: true }
